acd6f11c9bc9905561be901404a672b8984e4c2d
MadeReihan <reihanmade@gmail.com>
2022-08-27 22:32:54 +0700

    ui settings

--- lib/Home/setting_page.dart
+++ lib/Home/setting_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:madee_kan/Auth/login_page.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -10,6 +12,87 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Settings',
+          style: TextStyle(color: Colors.blue, fontSize: 22.sp),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.h,
+                    left: 24.w,
+                    right: 24.w,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Made Reihan Diva Suarna',
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10.h),
+                      Text(
+                        'Madee@gmail.com',
+                        style: TextStyle(
+                          fontSize: 22.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Card(
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () async {
+                    await Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                        (route) => false);
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 70.h,
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 24.w,
+                        right: 24.w,
+                      ),
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Logout',
+                          ),
+                          Icon(
+                            Icons.chevron_right,
+                            color: Colors.grey.shade700,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
